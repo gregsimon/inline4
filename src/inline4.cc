ab58ae0ffd94d5f827160ee2bb14a854a0b43d7a
Greg Simon <gregsimon@gmail.com>
2016-03-15 11:22:49 -0700

    stubbed out enough to link shell

--- src/inline4.cc
+++ src/inline4.cc
@@ -1,7 +1,288 @@
 #include "v8.h"
+#include "libplatform/libplatform.h"
 
-extern "C" {
 #include "duktape.h"
+
+namespace v8 {
+
+// -------------------------------------------------------------------------
+// class Platform
+class i4Platform : public Platform {
+public:
+  i4Platform() {}
+  size_t NumberOfAvailableBackgroundThreads() { return 0; }
+  void CallOnBackgroundThread(Task* task,
+                              ExpectedRuntime expected_runtime) override {
+    task->Run();
+    delete task;
+  }
+  void CallOnForegroundThread(v8::Isolate* isolate, Task* task) override {
+    task->Run();
+    delete task;
+  }
+  void CallDelayedOnForegroundThread(v8::Isolate* isolate, Task* task,
+                                     double delay_in_seconds) override {
+    delete task;
+  }
+  void CallIdleOnForegroundThread(v8::Isolate* isolate,
+                                  IdleTask* task) override {}
+  double MonotonicallyIncreasingTime() override {
+    return synthetic_time_in_sec_ += 0.00001;
+  }
+  bool IdleTasksEnabled(v8::Isolate* isolate) override { return false; }
+  uint64_t AddTraceEvent(char phase, const uint8_t* categoryEnabledFlag,
+                         const char* name, const char* scope, uint64_t id,
+                         uint64_t bind_id, int numArgs, const char** argNames,
+                         const uint8_t* argTypes, const uint64_t* argValues,
+                         unsigned int flags) override {
+    return 0;
+  }
+  void UpdateTraceEventDuration(const uint8_t* categoryEnabledFlag,
+                                const char* name, uint64_t handle) override {}
+  const uint8_t* GetCategoryGroupEnabled(const char* name) override {
+    static uint8_t no = 0;
+    return &no;
+  }
+
+  const char* GetCategoryGroupName(
+      const uint8_t* categoryEnabledFlag) override {
+    static const char* dummy = "dummy";
+    return dummy;
+  }
+private:
+  double synthetic_time_in_sec_ = 0.0;
+};
+
+namespace platform {
+  Platform* CreateDefaultPlatform(int thread_pool_size) {
+    return new i4Platform();
+  }
+  bool PumpMessageLoop(v8::Platform* platform, v8::Isolate* isolate) {
+    return true;
+  }
+}// namespace platform
+
+
+// -------------------------------------------------------------------------
+// class HandleScope
+HandleScope::HandleScope(Isolate* isolate) {
+}
+HandleScope::~HandleScope() {
+}
+
+// -------------------------------------------------------------------------
+// class ResourceConstraints
+ResourceConstraints::ResourceConstraints() {}
+void ResourceConstraints::ConfigureDefaults(uint64_t physical_memory,
+                         uint64_t virtual_memory_limit) {}
+
+// -------------------------------------------------------------------------
+// class Context
+
+/* static */ Local<Context> Context::New(
+      Isolate* isolate, ExtensionConfiguration* extensions,
+      Local<ObjectTemplate> global_template,
+      Local<Value> global_object)
+{
+}
+void Context::Enter() {
+}
+void Context::Exit() {
+}
+Isolate* Context::GetIsolate() {
+  return 0;
 }
 
 
+// -------------------------------------------------------------------------
+// class Value
+bool Value::IsTrue() const { return false; }
+/*bool Value::IsNull() const { return false; }
+bool Value::IsTrue() const { return false; }
+bool Value::IsFalse() const { return false; }
+bool Value::IsName() const { return false; }
+bool Value::IsString() const { return false; }
+bool Value::IsSymbol() const { return false; }
+bool Value::IsFunction() const { return false; }
+bool Value::IsArray() const { return false; }
+bool Value::IsObject() const { return false; }
+bool Value::IsBoolean() const { return false; }
+bool Value::IsNumber() const { return false; }
+bool Value::IsExternal() const { return false; }
+bool Value::IsInt32() const { return false; }
+bool Value::IsUint32() const { return false; }
+bool Value::IsDate() const { return false; }
+bool Value::IsArgumentsObject() const { return false; }
+bool Value::IsBooleanObject() const { return false; }
+bool Value::IsNumberObject() const { return false; }
+bool Value::IsStringObject() const { return false; }
+bool Value::IsSymbolObject() const { return false; }
+bool Value::IsNativeError() const { return false; }
+bool Value::IsRegExp() const { return false; }
+bool Value::IsGeneratorFunction() const { return false; }
+bool Value::IsGeneratorObject() const { return false; }*/
+Maybe<int32_t> Value::Int32Value(
+      Local<Context> context) const {
+}
+
+// -------------------------------------------------------------------------
+// class String
+/* static */MaybeLocal<String> String::NewFromUtf8(
+    Isolate* isolate, const char* data, v8::NewStringType type,
+    int length) {
+
+}
+int String::Length() const { return 0; }
+int String::Utf8Length() const { return 0; }
+bool String::IsOneByte() const { return false; }
+bool String::ContainsOnlyOneByte() const { return false; }
+
+
+// -------------------------------------------------------------------------
+// class String::Utf8value
+String::Utf8Value::Utf8Value(Local<v8::Value> obj) {
+}
+String::Utf8Value::~Utf8Value() {
+}
+
+// -------------------------------------------------------------------------
+// class Script
+/*static */ MaybeLocal<Script> Script::Compile(
+      Local<Context> context, Local<String> source,
+      ScriptOrigin* origin) {
+}
+/* static */ MaybeLocal<Value> Script::Run(Local<Context> context) {
+}
+
+
+// -------------------------------------------------------------------------
+// class Message
+Local<String> Message::Get() const { 
+}
+MaybeLocal<String> Message::GetSourceLine(
+      Local<Context> context) const {
+}
+ScriptOrigin Message::GetScriptOrigin() const {
+}
+Local<Value> Message::GetScriptResourceName() const {
+}
+Local<StackTrace> Message::GetStackTrace() const {
+}
+Maybe<int> Message::GetLineNumber(Local<Context> context) const {
+}
+int Message::GetStartPosition() const {
+  return 0;
+}
+int Message::GetEndPosition() const {
+  return 0;
+}
+Maybe<int> Message::GetStartColumn(Local<Context> context) const {
+}
+Maybe<int> Message::GetEndColumn(Local<Context> context) const {
+}
+
+// -------------------------------------------------------------------------
+// class V8
+void V8::SetNativesDataBlob(StartupData* startup_blob) {}
+void V8::SetSnapshotDataBlob(StartupData* startup_blob) {}
+StartupData V8::CreateSnapshotDataBlob(const char* custom_source) {
+  return StartupData{0,0};
+}
+void V8::InitializePlatform(Platform* platform) {}
+void V8::SetFlagsFromString(const char* str, int length) {}
+void V8::SetFlagsFromCommandLine(int* argc,
+                              char** argv,
+                              bool remove_flags) {}
+bool V8::Initialize() {
+  return true;
+}
+bool V8::InitializeICU(const char* icu_data_file) {
+  return true;
+}
+bool V8::Dispose() {
+  return true;
+}
+const char* V8::GetVersion() {
+  return "i4 JavaScript Engine";
+}
+void V8::ToLocalEmpty() {}
+void V8::FromJustIsNothing() {}
+void V8::ShutdownPlatform() {}
+void V8::InitializeExternalStartupData(const char* directory_path) {}
+void V8::InitializeExternalStartupData(const char* natives_blob,
+                                            const char* snapshot_blob) {}
+
+
+// -------------------------------------------------------------------------
+// class Template
+void Template::Set(Local<Name> name, Local<Data> value,
+           PropertyAttribute attributes) {
+}
+
+// -------------------------------------------------------------------------
+// class ObjectTemplate
+/* static */ Local<ObjectTemplate> ObjectTemplate::New(
+      Isolate* isolate,
+      Local<FunctionTemplate> constructor) {
+}
+
+
+// -------------------------------------------------------------------------
+// class FunctionTemplate
+/* static */ Local<FunctionTemplate> FunctionTemplate::New(
+      Isolate* isolate, FunctionCallback callback,
+      Local<Value> data,
+      Local<Signature> signature, int length) {
+
+}
+
+// -------------------------------------------------------------------------
+// class TryCatch
+TryCatch::TryCatch(Isolate* isolate) {
+}
+TryCatch::~TryCatch() {}
+MaybeLocal<Value> TryCatch::StackTrace(Local<Context> context) const {
+}
+Local<v8::Message> TryCatch::Message() const {
+}
+void TryCatch::Reset() {}
+void TryCatch::SetVerbose(bool) {}
+void TryCatch::SetCaptureMessage(bool) {}
+bool TryCatch::HasCaught() const { return false; }
+bool TryCatch::CanContinue() const { return true; }
+bool TryCatch::HasTerminated() const { return false; }
+Local<Value> TryCatch::ReThrow() {
+}
+Local<Value> TryCatch::Exception() const {
+}
+
+// -------------------------------------------------------------------------
+// class Isolate
+/* static */ Isolate* Isolate::New(const CreateParams& params) {
+
+}
+/* static */ Isolate* Isolate::GetCurrent() {
+  return 0;
+}
+void Isolate::Enter() {
+}
+void Isolate::Exit() {
+}
+void Isolate::Dispose() {
+}
+void Isolate::DiscardThreadSpecificMetadata() {}
+size_t Isolate::NumberOfHeapSpaces() {
+  return 0;
+}
+bool Isolate::InContext() {
+  return true;
+}
+Local<Value> Isolate::ThrowException(Local<Value> exception) {
+}
+Local<Context> Isolate::GetCurrentContext() {
+}
+bool Isolate::GetHeapSpaceStatistics(HeapSpaceStatistics* space_statistics,
+                              size_t index) { return 0; }
+size_t Isolate::NumberOfTrackedHeapObjectTypes() {}
+
+} //  namespace v8
--- src/test.cc
+++ src/test.cc
@@ -1,12 +1,259 @@
 #include "v8.h"
+#include "libplatform/libplatform.h"
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <fcntl.h>
 
+
+bool ExecuteString(v8::Isolate* isolate, v8::Local<v8::String> source,
+                   v8::Local<v8::Value> name, bool print_result,
+                   bool report_exceptions);
+void Print(const v8::FunctionCallbackInfo<v8::Value>& args);
+void Print(const v8::FunctionCallbackInfo<v8::Value>& args);
+void Read(const v8::FunctionCallbackInfo<v8::Value>& args);
+void Load(const v8::FunctionCallbackInfo<v8::Value>& args);
+void Quit(const v8::FunctionCallbackInfo<v8::Value>& args);
+void Version(const v8::FunctionCallbackInfo<v8::Value>& args);
+v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name);
+void ReportException(v8::Isolate* isolate, v8::TryCatch* handler);
+
+
+// Extracts a C string from a V8 Utf8Value.
+const char* ToCString(const v8::String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
+}
+
+
+
+
+class ShellArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
+ public:
+  virtual void* Allocate(size_t length) {
+    void* data = AllocateUninitialized(length);
+    return data == NULL ? data : memset(data, 0, length);
+  }
+  virtual void* AllocateUninitialized(size_t length) { return malloc(length); }
+  virtual void Free(void* data, size_t) { free(data); }
+};
+
+
+// Creates a new execution environment containing the built-in
+// functions.
+v8::Local<v8::Context> CreateShellContext(v8::Isolate* isolate) {
+  // Create a template for the global object.
+  v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
+  // Bind the global 'print' function to the C++ Print callback.
+  global->Set(
+      v8::String::NewFromUtf8(isolate, "print", v8::NewStringType::kNormal)
+          .ToLocalChecked(),
+      v8::FunctionTemplate::New(isolate, Print));
+  // Bind the global 'read' function to the C++ Read callback.
+  global->Set(v8::String::NewFromUtf8(
+                  isolate, "read", v8::NewStringType::kNormal).ToLocalChecked(),
+              v8::FunctionTemplate::New(isolate, Read));
+  // Bind the global 'load' function to the C++ Load callback.
+  global->Set(v8::String::NewFromUtf8(
+                  isolate, "load", v8::NewStringType::kNormal).ToLocalChecked(),
+              v8::FunctionTemplate::New(isolate, Load));
+  // Bind the 'quit' function
+  global->Set(v8::String::NewFromUtf8(
+                  isolate, "quit", v8::NewStringType::kNormal).ToLocalChecked(),
+              v8::FunctionTemplate::New(isolate, Quit));
+  // Bind the 'version' function
+  global->Set(
+      v8::String::NewFromUtf8(isolate, "version", v8::NewStringType::kNormal)
+          .ToLocalChecked(),
+      v8::FunctionTemplate::New(isolate, Version));
+
+  return v8::Context::New(isolate, NULL, global);
+}
+
+
+ShellArrayBufferAllocator array_buffer_allocator;
 
 int main(int argc, char** argv)
 {
+  v8::V8::InitializeICU();
+  v8::Platform* platform = v8::platform::CreateDefaultPlatform();
+  v8::V8::InitializePlatform(platform);
+  v8::V8::Initialize();
+  v8::V8::SetFlagsFromCommandLine(&argc, argv, true);
+
+  // create an isolate
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator = &array_buffer_allocator;
+  v8::Isolate* isolate = v8::Isolate::New(create_params);
+  v8::Isolate::Scope isolate_scope(isolate);
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = CreateShellContext(isolate);
+  if (context.IsEmpty()) {
+    fprintf(stderr, "Error creating context\n");
+    return 1;
+  }
+
+
   return 0;
+}
+
+
+
+// The callback that is invoked by v8 whenever the JavaScript 'print'
+// function is called.  Prints its arguments on stdout separated by
+// spaces and ending with a newline.
+void Print(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  bool first = true;
+  for (int i = 0; i < args.Length(); i++) {
+    v8::HandleScope handle_scope(args.GetIsolate());
+    if (first) {
+      first = false;
+    } else {
+      printf(" ");
+    }
+    v8::String::Utf8Value str(args[i]);
+    const char* cstr = ToCString(str);
+    printf("%s", cstr);
+  }
+  printf("\n");
+  fflush(stdout);
+}
+
+// The callback that is invoked by v8 whenever the JavaScript 'read'
+// function is called.  This function loads the content of the file named in
+// the argument into a JavaScript string.
+void Read(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  if (args.Length() != 1) {
+    args.GetIsolate()->ThrowException(
+        v8::String::NewFromUtf8(args.GetIsolate(), "Bad parameters",
+                                v8::NewStringType::kNormal).ToLocalChecked());
+    return;
+  }
+  v8::String::Utf8Value file(args[0]);
+  if (*file == NULL) {
+    args.GetIsolate()->ThrowException(
+        v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
+                                v8::NewStringType::kNormal).ToLocalChecked());
+    return;
+  }
+  v8::Local<v8::String> source;
+  if (!ReadFile(args.GetIsolate(), *file).ToLocal(&source)) {
+    args.GetIsolate()->ThrowException(
+        v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
+                                v8::NewStringType::kNormal).ToLocalChecked());
+    return;
+  }
+  args.GetReturnValue().Set(source);
+}
+
+
+// The callback that is invoked by v8 whenever the JavaScript 'load'
+// function is called.  Loads, compiles and executes its argument
+// JavaScript file.
+void Load(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  for (int i = 0; i < args.Length(); i++) {
+    v8::HandleScope handle_scope(args.GetIsolate());
+    v8::String::Utf8Value file(args[i]);
+    if (*file == NULL) {
+      args.GetIsolate()->ThrowException(
+          v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
+                                  v8::NewStringType::kNormal).ToLocalChecked());
+      return;
+    }
+    v8::Local<v8::String> source;
+    if (!ReadFile(args.GetIsolate(), *file).ToLocal(&source)) {
+      args.GetIsolate()->ThrowException(
+          v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
+                                  v8::NewStringType::kNormal).ToLocalChecked());
+      return;
+    }
+    if (!ExecuteString(args.GetIsolate(), source, args[i], false, false)) {
+      args.GetIsolate()->ThrowException(
+          v8::String::NewFromUtf8(args.GetIsolate(), "Error executing file",
+                                  v8::NewStringType::kNormal).ToLocalChecked());
+      return;
+    }
+  }
+}
+
+// The callback that is invoked by v8 whenever the JavaScript 'quit'
+// function is called.  Quits.
+void Quit(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  // If not arguments are given args[0] will yield undefined which
+  // converts to the integer value 0.
+  int exit_code =
+      args[0]->Int32Value(args.GetIsolate()->GetCurrentContext()).FromMaybe(0);
+  fflush(stdout);
+  fflush(stderr);
+  exit(exit_code);
+}
+
+
+void Version(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  args.GetReturnValue().Set(
+      v8::String::NewFromUtf8(args.GetIsolate(), v8::V8::GetVersion(),
+                              v8::NewStringType::kNormal).ToLocalChecked());
+}
+
+// Reads a file into a v8 string.
+v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name) {
+  FILE* file = fopen(name, "rb");
+  if (file == NULL) return v8::MaybeLocal<v8::String>();
+
+  fseek(file, 0, SEEK_END);
+  size_t size = ftell(file);
+  rewind(file);
+
+  char* chars = new char[size + 1];
+  chars[size] = '\0';
+  for (size_t i = 0; i < size;) {
+    i += fread(&chars[i], 1, size - i, file);
+    if (ferror(file)) {
+      fclose(file);
+      return v8::MaybeLocal<v8::String>();
+    }
+  }
+  fclose(file);
+  v8::MaybeLocal<v8::String> result = v8::String::NewFromUtf8(
+      isolate, chars, v8::NewStringType::kNormal, static_cast<int>(size));
+  delete[] chars;
+  return result;
+}
+
+// Executes a string within the current v8 context.
+bool ExecuteString(v8::Isolate* isolate, v8::Local<v8::String> source,
+                   v8::Local<v8::Value> name, bool print_result,
+                   bool report_exceptions) {
+  v8::HandleScope handle_scope(isolate);
+  v8::TryCatch try_catch(isolate);
+  v8::ScriptOrigin origin(name);
+  v8::Local<v8::Context> context(isolate->GetCurrentContext());
+  v8::Local<v8::Script> script;
+  if (!v8::Script::Compile(context, source, &origin).ToLocal(&script)) {
+    // Print errors that happened during compilation.
+    if (report_exceptions)
+      ReportException(isolate, &try_catch);
+    return false;
+  } else {
+    v8::Local<v8::Value> result;
+    if (!script->Run(context).ToLocal(&result)) {
+      assert(try_catch.HasCaught());
+      // Print errors that happened during execution.
+      if (report_exceptions)
+        ReportException(isolate, &try_catch);
+      return false;
+    } else {
+      assert(!try_catch.HasCaught());
+      if (print_result && !result->IsUndefined()) {
+        // If all went well and the result wasn't undefined then print
+        // the returned value.
+        v8::String::Utf8Value str(result);
+        const char* cstr = ToCString(str);
+        printf("%s\n", cstr);
+      }
+      return true;
+    }
+  }
 }
 
