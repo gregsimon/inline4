3c64df148a6a2de7e0a1430cf2d60499a6a4c2d6
Greg Simon <gregsimon@gmail.com>
2016-03-15 12:21:21 -0700

    added hello world as test.cc

--- src/test.cc
+++ src/test.cc
@@ -1,35 +1,15 @@
-#include "v8.h"
-#include "libplatform/libplatform.h"
-
+// Copyright 2015 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <fcntl.h>
+#include <string.h>
+#include "include/libplatform/libplatform.h"
+#include "include/v8.h"
 
 
-bool ExecuteString(v8::Isolate* isolate, v8::Local<v8::String> source,
-                   v8::Local<v8::Value> name, bool print_result,
-                   bool report_exceptions);
-void Print(const v8::FunctionCallbackInfo<v8::Value>& args);
-void Print(const v8::FunctionCallbackInfo<v8::Value>& args);
-void Read(const v8::FunctionCallbackInfo<v8::Value>& args);
-void Load(const v8::FunctionCallbackInfo<v8::Value>& args);
-void Quit(const v8::FunctionCallbackInfo<v8::Value>& args);
-void Version(const v8::FunctionCallbackInfo<v8::Value>& args);
-v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name);
-void ReportException(v8::Isolate* isolate, v8::TryCatch* handler);
-
-
-// Extracts a C string from a V8 Utf8Value.
-const char* ToCString(const v8::String::Utf8Value& value) {
-  return *value ? *value : "<string conversion failed>";
-}
-
-
-
-
-class ShellArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
+using namespace v8;
+class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
  public:
   virtual void* Allocate(size_t length) {
     void* data = AllocateUninitialized(length);
@@ -38,222 +18,42 @@ class ShellArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
   virtual void* AllocateUninitialized(size_t length) { return malloc(length); }
   virtual void Free(void* data, size_t) { free(data); }
 };
-
-
-// Creates a new execution environment containing the built-in
-// functions.
-v8::Local<v8::Context> CreateShellContext(v8::Isolate* isolate) {
-  // Create a template for the global object.
-  v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-  // Bind the global 'print' function to the C++ Print callback.
-  global->Set(
-      v8::String::NewFromUtf8(isolate, "print", v8::NewStringType::kNormal)
-          .ToLocalChecked(),
-      v8::FunctionTemplate::New(isolate, Print));
-  // Bind the global 'read' function to the C++ Read callback.
-  global->Set(v8::String::NewFromUtf8(
-                  isolate, "read", v8::NewStringType::kNormal).ToLocalChecked(),
-              v8::FunctionTemplate::New(isolate, Read));
-  // Bind the global 'load' function to the C++ Load callback.
-  global->Set(v8::String::NewFromUtf8(
-                  isolate, "load", v8::NewStringType::kNormal).ToLocalChecked(),
-              v8::FunctionTemplate::New(isolate, Load));
-  // Bind the 'quit' function
-  global->Set(v8::String::NewFromUtf8(
-                  isolate, "quit", v8::NewStringType::kNormal).ToLocalChecked(),
-              v8::FunctionTemplate::New(isolate, Quit));
-  // Bind the 'version' function
-  global->Set(
-      v8::String::NewFromUtf8(isolate, "version", v8::NewStringType::kNormal)
-          .ToLocalChecked(),
-      v8::FunctionTemplate::New(isolate, Version));
-
-  return v8::Context::New(isolate, NULL, global);
-}
-
-
-ShellArrayBufferAllocator array_buffer_allocator;
-
-int main(int argc, char** argv)
-{
-  v8::V8::InitializeICU();
-  v8::Platform* platform = v8::platform::CreateDefaultPlatform();
-  v8::V8::InitializePlatform(platform);
-  v8::V8::Initialize();
-  v8::V8::SetFlagsFromCommandLine(&argc, argv, true);
-
-  // create an isolate
-  v8::Isolate::CreateParams create_params;
-  create_params.array_buffer_allocator = &array_buffer_allocator;
-  v8::Isolate* isolate = v8::Isolate::New(create_params);
-  v8::Isolate::Scope isolate_scope(isolate);
-  v8::HandleScope handle_scope(isolate);
-  v8::Local<v8::Context> context = CreateShellContext(isolate);
-  if (context.IsEmpty()) {
-    fprintf(stderr, "Error creating context\n");
-    return 1;
+int main(int argc, char* argv[]) {
+  // Initialize V8.
+  V8::InitializeICU();
+  V8::InitializeExternalStartupData(argv[0]);
+  Platform* platform = platform::CreateDefaultPlatform();
+  V8::InitializePlatform(platform);
+  V8::Initialize();
+  // Create a new Isolate and make it the current one.
+  ArrayBufferAllocator allocator;
+  Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator = &allocator;
+  Isolate* isolate = Isolate::New(create_params);
+  {
+    Isolate::Scope isolate_scope(isolate);
+    // Create a stack-allocated handle scope.
+    HandleScope handle_scope(isolate);
+    // Create a new context.
+    Local<Context> context = Context::New(isolate);
+    // Enter the context for compiling and running the hello world script.
+    Context::Scope context_scope(context);
+    // Create a string containing the JavaScript source code.
+    Local<String> source =
+        String::NewFromUtf8(isolate, "'Hello' + ', World!'",
+                            NewStringType::kNormal).ToLocalChecked();
+    // Compile the source code.
+    Local<Script> script = Script::Compile(context, source).ToLocalChecked();
+    // Run the script to get the result.
+    Local<Value> result = script->Run(context).ToLocalChecked();
+    // Convert the result to an UTF8 string and print it.
+    String::Utf8Value utf8(result);
+    printf("%s\n", *utf8);
   }
-
-
+  // Dispose the isolate and tear down V8.
+  isolate->Dispose();
+  V8::Dispose();
+  V8::ShutdownPlatform();
+  delete platform;
   return 0;
 }
-
-
-
-// The callback that is invoked by v8 whenever the JavaScript 'print'
-// function is called.  Prints its arguments on stdout separated by
-// spaces and ending with a newline.
-void Print(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  bool first = true;
-  for (int i = 0; i < args.Length(); i++) {
-    v8::HandleScope handle_scope(args.GetIsolate());
-    if (first) {
-      first = false;
-    } else {
-      printf(" ");
-    }
-    v8::String::Utf8Value str(args[i]);
-    const char* cstr = ToCString(str);
-    printf("%s", cstr);
-  }
-  printf("\n");
-  fflush(stdout);
-}
-
-// The callback that is invoked by v8 whenever the JavaScript 'read'
-// function is called.  This function loads the content of the file named in
-// the argument into a JavaScript string.
-void Read(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  if (args.Length() != 1) {
-    args.GetIsolate()->ThrowException(
-        v8::String::NewFromUtf8(args.GetIsolate(), "Bad parameters",
-                                v8::NewStringType::kNormal).ToLocalChecked());
-    return;
-  }
-  v8::String::Utf8Value file(args[0]);
-  if (*file == NULL) {
-    args.GetIsolate()->ThrowException(
-        v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
-                                v8::NewStringType::kNormal).ToLocalChecked());
-    return;
-  }
-  v8::Local<v8::String> source;
-  if (!ReadFile(args.GetIsolate(), *file).ToLocal(&source)) {
-    args.GetIsolate()->ThrowException(
-        v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
-                                v8::NewStringType::kNormal).ToLocalChecked());
-    return;
-  }
-  args.GetReturnValue().Set(source);
-}
-
-
-// The callback that is invoked by v8 whenever the JavaScript 'load'
-// function is called.  Loads, compiles and executes its argument
-// JavaScript file.
-void Load(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  for (int i = 0; i < args.Length(); i++) {
-    v8::HandleScope handle_scope(args.GetIsolate());
-    v8::String::Utf8Value file(args[i]);
-    if (*file == NULL) {
-      args.GetIsolate()->ThrowException(
-          v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
-                                  v8::NewStringType::kNormal).ToLocalChecked());
-      return;
-    }
-    v8::Local<v8::String> source;
-    if (!ReadFile(args.GetIsolate(), *file).ToLocal(&source)) {
-      args.GetIsolate()->ThrowException(
-          v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file",
-                                  v8::NewStringType::kNormal).ToLocalChecked());
-      return;
-    }
-    if (!ExecuteString(args.GetIsolate(), source, args[i], false, false)) {
-      args.GetIsolate()->ThrowException(
-          v8::String::NewFromUtf8(args.GetIsolate(), "Error executing file",
-                                  v8::NewStringType::kNormal).ToLocalChecked());
-      return;
-    }
-  }
-}
-
-// The callback that is invoked by v8 whenever the JavaScript 'quit'
-// function is called.  Quits.
-void Quit(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  // If not arguments are given args[0] will yield undefined which
-  // converts to the integer value 0.
-  int exit_code =
-      args[0]->Int32Value(args.GetIsolate()->GetCurrentContext()).FromMaybe(0);
-  fflush(stdout);
-  fflush(stderr);
-  exit(exit_code);
-}
-
-
-void Version(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  args.GetReturnValue().Set(
-      v8::String::NewFromUtf8(args.GetIsolate(), v8::V8::GetVersion(),
-                              v8::NewStringType::kNormal).ToLocalChecked());
-}
-
-// Reads a file into a v8 string.
-v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name) {
-  FILE* file = fopen(name, "rb");
-  if (file == NULL) return v8::MaybeLocal<v8::String>();
-
-  fseek(file, 0, SEEK_END);
-  size_t size = ftell(file);
-  rewind(file);
-
-  char* chars = new char[size + 1];
-  chars[size] = '\0';
-  for (size_t i = 0; i < size;) {
-    i += fread(&chars[i], 1, size - i, file);
-    if (ferror(file)) {
-      fclose(file);
-      return v8::MaybeLocal<v8::String>();
-    }
-  }
-  fclose(file);
-  v8::MaybeLocal<v8::String> result = v8::String::NewFromUtf8(
-      isolate, chars, v8::NewStringType::kNormal, static_cast<int>(size));
-  delete[] chars;
-  return result;
-}
-
-// Executes a string within the current v8 context.
-bool ExecuteString(v8::Isolate* isolate, v8::Local<v8::String> source,
-                   v8::Local<v8::Value> name, bool print_result,
-                   bool report_exceptions) {
-  v8::HandleScope handle_scope(isolate);
-  v8::TryCatch try_catch(isolate);
-  v8::ScriptOrigin origin(name);
-  v8::Local<v8::Context> context(isolate->GetCurrentContext());
-  v8::Local<v8::Script> script;
-  if (!v8::Script::Compile(context, source, &origin).ToLocal(&script)) {
-    // Print errors that happened during compilation.
-    if (report_exceptions)
-      ReportException(isolate, &try_catch);
-    return false;
-  } else {
-    v8::Local<v8::Value> result;
-    if (!script->Run(context).ToLocal(&result)) {
-      assert(try_catch.HasCaught());
-      // Print errors that happened during execution.
-      if (report_exceptions)
-        ReportException(isolate, &try_catch);
-      return false;
-    } else {
-      assert(!try_catch.HasCaught());
-      if (print_result && !result->IsUndefined()) {
-        // If all went well and the result wasn't undefined then print
-        // the returned value.
-        v8::String::Utf8Value str(result);
-        const char* cstr = ToCString(str);
-        printf("%s\n", cstr);
-      }
-      return true;
-    }
-  }
-}
-
